40eb9154b63676506dd1e69af682db8792130a0e
kazekim <jirawat.h@kazekim.com>
2018-07-30 13:19:17 +0700

    variables section

---
+++ 03_Variables/03_Less-Emphasis/08_ExerciseYourName/01_OneSolution/MyNameVar.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+var name = "Kim"
+
+func main() {
+	fmt.Println("Hello, ", name)
+}
---
+++ 03_Variables/03_Less-Emphasis/08_ExerciseYourName/03_AnotherSolution/MyNameVar.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Kim"
+	fmt.Println("Hello, ", name)
+}
